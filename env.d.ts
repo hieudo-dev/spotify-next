0820021b19cde94da1a0302116fd344f5d17c717
Hieu Do <hieudo1@beeketing.net>
2023-04-18 23:13:43 +0700

    feat: expose access token to client's session

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,13 @@
+import { Session } from "next-auth";
+
 namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_SPOTIFY_BASE_API: string;
+  }
+}
+
+declare module "next-auth" {
+  interface Session {
+    accessToken: string;
   }
 }
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,24 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
   providers: [
     SpotifyProvider({
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      return session;
+    },
+    async jwt({ token, account, session, user }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+  },
 };
 export default NextAuth(authOptions);
