a904215238c41af70a52cffbc6dd163e13bf2860
Hieu Do <hieudo1@beeketing.net>
2023-04-19 15:25:28 +0700

    build: add react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,33 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import "~assets/styles/globals.css";
 import Header from "~components/Header";
 import Navigation from "~components/Navigation";
 
+const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <div className="flex flex-col h-screen max-h-screen font-sans bg-black text-slate-200">
-        <div className="flex flex-1 h-[calc(100%-88px)]">
-          <Navigation />
-          <div className="w-full bg-slate-950">
-            <Header />
-            <main className="flex-1 p-2">
-              <Component {...pageProps} />
-            </main>
+      <QueryClientProvider client={queryClient}>
+        <div className="flex flex-col h-screen max-h-screen font-sans bg-black text-slate-200">
+          <div className="flex flex-1 h-[calc(100%-88px)]">
+            <Navigation />
+            <div className="w-full bg-slate-950">
+              <Header />
+              <main className="flex-1 p-2">
+                <Component {...pageProps} />
+              </main>
+            </div>
           </div>
-        </div>
 
-        <footer className="p-4">
-          <div className="flex items-center justify-between h-14">
-            <p>Footer</p>
-          </div>
-        </footer>
-      </div>
+          <footer className="p-4">
+            <div className="flex items-center justify-between h-14">
+              <p>Footer</p>
+            </div>
+          </footer>
+        </div>
+      </QueryClientProvider>
     </SessionProvider>
   );
 }
