face42b4bbc40a1fd4894c2520048502a61b86dc
Hieu Do <hieudo1@beeketing.net>
2023-04-19 23:57:07 +0700

    feat: update auth api expose user id to client's session

--- env.d.ts
+++ env.d.ts
@@ -9,5 +9,8 @@ namespace NodeJS {
 declare module "next-auth" {
   interface Session {
     accessToken: string;
+    user: {
+      id: string;
+    };
   }
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,21 +1,29 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
+const SCOPE =
+  "streaming user-read-private user-read-email user-read-recently-played user-read-playback-state user-modify-playback-state user-library-modify user-follow-modify playlist-read-private playlist-modify-public playlist-modify-private user-top-read";
+
 export const authOptions: AuthOptions = {
   providers: [
     SpotifyProvider({
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
+      authorization:
+        "https://accounts.spotify.com/authorize?show_dialog=true&scope=" +
+        encodeURI(SCOPE),
     }),
   ],
   callbacks: {
     session({ session, token }) {
       session.accessToken = token.accessToken as string;
+      session.user.id = token.userId as string;
       return session;
     },
-    async jwt({ token, account, session, user }) {
+    async jwt({ token, account, user }) {
       if (account) {
         token.accessToken = account.access_token;
+        token.userId = user.id;
       }
       return token;
     },
