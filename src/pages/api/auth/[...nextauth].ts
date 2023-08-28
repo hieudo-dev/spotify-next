a0deaf177e2cf370363aad517c130f93c343a2d3
Hieu Do <hieudo1@beeketing.net>
2023-08-28 22:43:20 +0700

    chore: remove unnecessary spotify api scopes

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,15 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
-const SCOPE =
-  "streaming user-read-private user-library-read user-read-email user-read-recently-played user-read-playback-state user-modify-playback-state user-library-modify user-follow-modify playlist-read-private playlist-modify-public playlist-modify-private user-top-read";
+const SCOPE = [
+  "streaming",
+  "user-read-private",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "user-library-modify",
+  "playlist-read-private",
+  "user-top-read",
+].join(" ");
 
 export const authOptions: AuthOptions = {
   providers: [
